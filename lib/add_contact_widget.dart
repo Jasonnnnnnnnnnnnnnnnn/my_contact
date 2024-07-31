9d059d89d5f9620f5a7f3365ad7f436bc5aa70de
Thitiphong Nimmak <64160048@go.buu.ac.th>
2024-08-01 01:27:55 +0700

    setting OutlineInputBorder

--- lib/add_contact_widget.dart
+++ lib/add_contact_widget.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class AddContactWidget extends StatefulWidget {
   const AddContactWidget({Key? key}) : super(key: key);
@@ -8,11 +10,39 @@ class AddContactWidget extends StatefulWidget {
 }
 
 class _AddContactWidgetState extends State<AddContactWidget> {
+  final _formkey = GlobalKey<FormState>();
+  
+ late TextEditingController nameController;
+ @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    nameController = TextEditingController();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Add New Contact'),
+      ),
+      body: Form(
+        key: _formkey,
+        child: ListView(
+          padding: EdgeInsets.all(16),
+          children: [
+            TextFormField(
+              controller: nameController,
+              decoration: InputDecoration(
+                  labelText: 'Name', border: OutlineInputBorder()),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter name';
+                }
+                return null;
+              },
+            )
+          ],
+        ),
       ),
     );
   }
